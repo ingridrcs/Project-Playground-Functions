7e82cc08bdbb2e6863b08ecf4e0a08a4d83825b8
Ingrid_Ramos <ingridrcs@gmail.com>
2021-11-11 21:34:13 -0300

    Desafio 5

--- src/challenges.js
+++ src/challenges.js
@@ -7,7 +7,7 @@ function compareTrue() {
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  let area= (base*height)/2;
+  let area = (base * height) / 2;
   return area;
 }
 
@@ -15,24 +15,24 @@ function calcArea(base, height) {
 // Desafio 3 //*Source: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254 */
 function splitSentence(frase) {
   // seu código aqui
-  let resultado = frase.split(" ");
+  let resultado = frase.split(' ');
   return resultado;
 }
 
 // Desafio 4 //*Source: estava com dúvida de como escrever o resultado e consegui solucionar através do slack https://trybecourse.slack.com/archives/C02HY11SPJP/p1636663927349400?thread_ts=1636663852.348500&cid=C02HY11SPJP*/
 function concatName(lista) {
   // seu código aqui
- let num = lista.length -1;
- let resultado = `${lista[num]}, ${lista[0]}`;
- return resultado;
+  let num = lista.length - 1;
+  let resultado = `${lista[num]}, ${lista[0]}`;
+  return resultado;
 }
 
 
 // Desafio 5
-function footballPoints(wins,ties) {
+function footballPoints(wins, ties) {
   // seu código aqui
-  let pontos = (wins*3) + ties;
-  console.log(pontos);
+  let pontos = (wins * 3) + ties;
+  return pontos;
 }
 
 // Desafio 6
