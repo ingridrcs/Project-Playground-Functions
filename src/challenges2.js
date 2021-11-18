b4e8ac6c7d196d3c74ef11611a818afcc07a7c85
Ingrid_Ramos <ingridrcs@gmail.com>
2021-11-18 16:32:50 -0300

    Parte do desafio11 com erros

--- src/challenges2.js
+++ src/challenges2.js
@@ -16,9 +16,28 @@ function techList(tech, name) {
   return (listaNova);
 }
 
-// Desafio 11
+// Desafio 11 Source:https://www.alura.com.br/artigos/javascript-replace-manipulando-strings-e-regex e https://blog.betrybe.com/javascript/javascript-replace/
 function generatePhoneNumber(telefone) {
   // seu código aqui
+    let soma=0;
+if (telefone.length !==11) {
+    return('Array com tamanho incorreto.');
+}
+for (let i=0;i<telefone.length;i+=1) {
+        if (telefone[i]<0 || telefone[i]>9) {
+        return('não é possível gerar um número de telefone com esses valores');
+    }   
+        for(let num=0;num<telefone.length;num+=1) {
+            if (telefone[i]===telefone[num]){
+                soma +=1;
+            }
+            }
+        if (soma>=3) {
+            return("não é possível gerar um número de telefone com esses valores");
+        } 
+      }
+    
+return('(telefone[0]+telefone[1])+" "+telefone[2]+telefone[3]+telefone[4]+telefone[5]+"-"+telefone[6]+telefone[7]+telefone[8]+telefone[9]+telefone[10]');
 }
 
 // Desafio 12 - Source: Math.abs https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
