7410a0bcd2f1bf1ff7a7cf62f78b23d3d0a9e9a4
Ingrid_Ramos <ingridrcs@gmail.com>
2021-11-18 18:07:40 -0300

    Reformulando o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -19,25 +19,24 @@ function techList(tech, name) {
 // Desafio 11 Source:https://www.alura.com.br/artigos/javascript-replace-manipulando-strings-e-regex e https://blog.betrybe.com/javascript/javascript-replace/
 function generatePhoneNumber(telefone) {
   // seu código aqui
-    let soma=0;
-if (telefone.length !==11) {
-    return('Array com tamanho incorreto.');
-}
-for (let i=0;i<telefone.length;i+=1) {
-        if (telefone[i]<0 || telefone[i]>9) {
-        return('não é possível gerar um número de telefone com esses valores');
-    }   
-        for(let num=0;num<telefone.length;num+=1) {
-            if (telefone[i]===telefone[num]){
-                soma +=1;
-            }
-            }
-        if (soma>=3) {
-            return("não é possível gerar um número de telefone com esses valores");
-        } 
+  let soma = 0;
+  if (telefone.length !== 11) {
+    return ('Array com tamanho incorreto.');
+  }
+  for (let i = 0; i < telefone.length; i += 1) {
+    if (telefone[i] < 0 || telefone[i] > 9) {
+      return ('não é possível gerar um número de telefone com esses valores');
+    }
+    for (let num = 0; num < telefone.length; num += 1) {
+      if (telefone[i] === telefone[num]) {
+        soma += 1;
       }
-    
-return('(telefone[0]+telefone[1])+" "+telefone[2]+telefone[3]+telefone[4]+telefone[5]+"-"+telefone[6]+telefone[7]+telefone[8]+telefone[9]+telefone[10]');
+       if (soma >2) {
+      return ('não é possível gerar um número de telefone com esses valores');
+    }
+    }
+  }
+  return ('(telefone[0]+telefone[1])+" "+telefone[2]+telefone[3]+telefone[4]+telefone[5]+"-"+telefone[6]+telefone[7]+telefone[8]+telefone[9]+telefone[10]');
 }
 
 // Desafio 12 - Source: Math.abs https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
@@ -54,23 +53,23 @@ function triangleCheck(lineA, lineB, lineC) {
   }
 }
 
-// Desafio 13 Tirar os números de dentro de uma string:https://stackoverflow.com/questions/10003683/how-can-i-extract-a-number-from-a-string-in-javascript    
+// Desafio 13 Tirar os números de dentro de uma string:https://stackoverflow.com/questions/10003683/how-can-i-extract-a-number-from-a-string-in-javascript
 // Transformar string em array:https://pt.stackoverflow.com/questions/172749/como-transformar-string-em-array-de-caracteres
 // Uso do parseInt: https://pt.stackoverflow.com/questions/90841/somar-array-javascript e https://www.youtube.com/watch?v=w14BIa5fOuU
 function hydrate(string) {
   // seu código aqui
-  let final=0;
+  let final = 0;
   let novo = string.replace(/[^0-9]/g,'');
-  let array = novo.split ("");
-for (let i = 0; i < array.length; i += 1) {
-final += parseInt(array[i]);
-}
-if (final === 1) {
-    return (final+" "+ 'copo de água');
-}
-else {
-    return (final+" "+ 'copos de água');
-}
+  let array = novo.split('');
+  for (let i = 0; i < array.length; i += 1) {
+    final += parseInt(array[i]);
+  }
+  if (final === 1) {
+    return (final + ' ' + 'copo de água');
+  }
+  else {
+    return (final + ' ' + 'copos de água');
+  }
 }
 
 module.exports = {
